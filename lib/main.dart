3597845d801c762b3a4b9c97107327ac368ede85
yxjie <yangxiangjie@koudailc.com>
2020-06-19 18:42:28 +0800

    [NFF] 修改应用名称

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter框架',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         //定义光标颜色
